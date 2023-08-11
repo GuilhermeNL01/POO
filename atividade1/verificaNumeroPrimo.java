400435df4145f674eb629e5972e278394b5652a1
GuilhermeNL01 <guilherme.lobo@a.ucb.br>
2023-08-11 10:39:30 -0300

    added verificarNumeroPrimo.java

---
+++ atividade1/verificaNumeroPrimo.java
@@ -0,0 +1,37 @@
+package atividade1;
+
+public class verificaNumeroPrimo {
+
+    public static void main(String args[]){
+        System.out.println("Ola mundo!");
+        verificaNumeroPrimo(1232132131);
+
+    }
+public static void verificaNumeroPrimo(Integer n ){
+
+}
+}
+
+ 
+    
+
+
+    
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
