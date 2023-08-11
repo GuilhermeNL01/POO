1655091e2dd6a24574be9029dd465f58ac90b3c4
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2023-08-11 14:05:14 -0300

    test

--- atividade1/verificaNumeroPrimo.java
+++ atividade1/verificaNumeroPrimo.java
@@ -2,14 +2,14 @@ package atividade1;
 
 public class verificaNumeroPrimo {
 
-    public static void main(String args[]){
-        System.out.println("Ola mundo!");
-        verificaNumeroPrimo(1232132131);
+   public static void main(String args[]) {
+    System.out.println("Ola mundo!");
+    verificaNumeroPrimo(1232132131);
 
-    }
-public static void verificaNumeroPrimo(Integer n ){
+   }
+   public static void verificaNumeroPrimo(Integer n ){
 
-}
+   }
 }
 
  
