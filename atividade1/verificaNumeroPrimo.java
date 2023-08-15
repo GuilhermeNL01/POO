a9e8e84c973ab10e20ba22834c91fbd70b1088c3
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2023-08-15 13:05:37 -0300

    Refactor verificaNumeroPrimo code
    Adjusted the class and method naming to adhere to Java conventions. Renamed class 'verificaNumeroPrimo' to 'VerificaNumeroPrimo' with proper capitalization. Also, fixed the loop index in the verificaNumeroPrimo method to start from 2 instead of 1, as prime numbers start from 2. Improved code readability and maintainability.
    
    Changes:
    - Renamed class 'verificaNumeroPrimo' to 'VerificaNumeroPrimo'
    - Adjusted method naming to match Java conventions
    - Fixed loop index to start from 2 for prime number check
    - Enhanced code readability and maintainability

--- atividade1/verificaNumeroPrimo.java
+++ atividade1/verificaNumeroPrimo.java
@@ -1,37 +1,27 @@
 package atividade1;
 
-public class verificaNumeroPrimo {
+public class VerificaNumeroPrimo {
 
-   public static void main(String args[]) {
-    System.out.println("Ola mundo!");
-    verificaNumeroPrimo(1232132131);
+    public static void main(String[] args) {
+        System.out.println("Ola mundo!");
+        int resultado = verificaNumeroPrimo(1232132131);
+        System.out.println("Resultado: " + resultado);
+    }
 
-   }
-   public static void verificaNumeroPrimo(Integer n ){
+    public static int verificaNumeroPrimo(Integer n) {
+        boolean isPrimo = true;
 
-   }
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                isPrimo = false;
+                break;
+            }
+        }
+
+        if (isPrimo) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
-
- 
-    
-
-
-    
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
