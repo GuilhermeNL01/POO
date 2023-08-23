1a3e1c4e3bb670474b4855a84a06ec0a14e115af
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2023-08-23 10:41:40 -0300

    added folder atividade 2, and added to this folder Exercício 1 and Exercício 2

---
+++ atividade2/Exercicio1.java
@@ -0,0 +1,50 @@
+package atividade2;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicita ao usuário para inserir o primeiro número
+        System.out.print("Digite o primeiro número: ");
+        double numero1 = scanner.nextDouble();
+
+        // Solicita ao usuário para inserir o segundo número
+        System.out.print("Digite o segundo número: ");
+        double numero2 = scanner.nextDouble();
+
+        // Solicita ao usuário para inserir a operação desejada (+, -, *, /)
+        System.out.print("Digite a operação (+, -, *, /): ");
+        char operacao = scanner.next().charAt(0);
+
+        double resultado = 0;
+
+        // Realiza a operação selecionada pelo usuário
+        switch (operacao) {
+            case '+':
+                resultado = numero1 + numero2; // Soma
+                break;
+            case '-':
+                resultado = numero1 - numero2; // Subtração
+                break;
+            case '*':
+                resultado = numero1 * numero2; // Multiplicação
+                break;
+            case '/':
+                if (numero2 != 0) {
+                    resultado = numero1 / numero2; // Divisão, evita divisão por zero
+                } else {
+                    System.out.println("Erro: Divisão por zero.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Operação inválida.");
+                return;
+        }
+
+        // Exibe o resultado da operação
+        System.out.println("Resultado: " + resultado);
+    }
+}
---
+++ atividade2/Exercicio2.java
@@ -0,0 +1,34 @@
+package atividade2;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        // Cria um objeto Scanner para receber entrada do usuário
+        Scanner scanner = new Scanner(System.in);
+
+        // Mostra as opções de conversão
+        System.out.println("Escolha a direção da conversão:");
+        System.out.println("1. Celsius para Fahrenheit");
+        System.out.println("2. Fahrenheit para Celsius");
+        int escolha = scanner.nextInt(); // Lê a escolha do usuário
+
+        double temperatura;
+        double resultado;
+
+        // Verifica a escolha do usuário
+        if (escolha == 1) {
+            System.out.print("Digite a temperatura em Celsius: ");
+            temperatura = scanner.nextDouble(); // Lê a temperatura em Celsius
+            resultado = (temperatura * 9 / 5) + 32; // Converte para Fahrenheit
+            System.out.println("Temperatura em Fahrenheit: " + resultado); // Mostra o resultado
+        } else if (escolha == 2) {
+            System.out.print("Digite a temperatura em Fahrenheit: ");
+            temperatura = scanner.nextDouble(); // Lê a temperatura em Fahrenheit
+            resultado = (temperatura - 32) * 5 / 9; // Converte para Celsius
+            System.out.println("Temperatura em Celsius: " + resultado); // Mostra o resultado
+        } else {
+            System.out.println("Escolha inválida."); // Mostra mensagem de erro para escolha inválida
+        }
+    }
+}
